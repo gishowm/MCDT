a4ecdbdcca871d6b036b2658436d9b80f0a6b831
hewanchun <gishowm@outlook.com>
2020-09-26 16:53:20 +0800

    Add methods  for example  String,Int,ToString etc to object  of JSON.

---
+++ Current.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+using System.Web.SessionState;
+
+public static class Current
+{
+    public static HttpRequest Request
+    {
+        get
+        {
+            return HttpContext.Current.Request;
+        }
+    }
+    public static HttpResponse Response
+    {
+        get
+        {
+            return HttpContext.Current.Response;
+        }
+    }
+    public static HttpSessionState Session
+    {
+        get
+        {
+            return HttpContext.Current.Session;
+        }
+    }
+    public static HttpServerUtility Server
+    {
+        get
+        {
+            return HttpContext.Current.Server;
+        }
+    }
+}
--- JSON.cs
+++ JSON.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -28,6 +29,46 @@ namespace MCDT
                 json[item.Key] = item.Value;
             }
             return json;
+        }
+
+        public string String(string key)
+        {
+            if (!this.Keys.Contains(key)) throw new Exception("Key:" + key + "Is Not Found");
+            return this[key].ToString();
+        }
+        public int Int(string key)
+        {
+            if (!this.Keys.Contains(key)) throw new Exception("Key:" + key + "Is Not Found");
+            return Convert.ToInt32(this[key]);
+
+        }
+        public decimal Decimal(string key)
+        {
+            if (!this.Keys.Contains(key)) throw new Exception("Key:" + key + "Is Not Found");
+            return Convert.ToDecimal(this[key]);
+        }
+
+        public double Double(string key)
+        {
+            if (!this.Keys.Contains(key)) throw new Exception("Key:" + key + "Is Not Found");
+            return Convert.ToDouble(this[key]);
+        }
+        public bool Boolean(string key)
+        {
+            if (!this.Keys.Contains(key)) throw new Exception("Key:" + key + "Is Not Found");
+            return Convert.ToBoolean(this[key]);
+        }
+
+        public string ToString()
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("转换JsonString失败:" + ex.Message);
+            }
         }
 
     }
